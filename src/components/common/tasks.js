7343fdc338d0b89cb9a61f6ee8022b5d3b7f58f7
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-05-16 12:07:53 +0200

    added task component and styles for it

--- src/components/common/tasks.js
+++ src/components/common/tasks.js
@@ -1,29 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Tasks({ tasks }) {
-  const formatedDate = (oldDate) => {
-    const date = new Date(oldDate)
+import './Tasks.scss';
 
-    return date.toLocaleString()
-  }
+import editSVG from '../../assets/edit.svg';
+import deleteSVG from '../../assets/delete.svg';
 
+export default function Tasks({ tasks, formatedDate }) {
   return (
-    <ul>
+    <ul className="tasks-list">
       {tasks.map((task) => {
         return (
-          <li key = {task._id}>
-            <h2>
-              {task.title}
-            </h2>
-            {task.desc 
-              ? <p>{task.desc}</p> 
-              : '' }
-            <time>
-              {formatedDate(task.createdAt)}
-            </time>
+          <li key={task._id} className="task">
+            <div className="task__content-container">
+              <h2 className="task__header">
+                <Link to={task._id}>{task.title}</Link>
+              </h2>
+              {task.desc ? (
+                <p className="task__description">{task.desc}</p>
+              ) : (
+                ''
+              )}
+              <time className="task__timestamp">
+                {formatedDate(task.createdAt)}
+              </time>
+            </div>
+            <div className="task__button-container">
+              <Link to={task._id} className="task__edit-button">
+                <img src={editSVG}></img>
+              </Link>
+              <button className="task__delete-button">
+                <img src={deleteSVG}></img>
+              </button>
+            </div>
           </li>
-        )
+        );
       })}
     </ul>
-  )
+  );
 }
