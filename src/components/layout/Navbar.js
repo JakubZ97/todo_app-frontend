c1b971ce262dbb63b762c8abe30e2fb15c160eee
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-05-16 12:06:53 +0200

    preattier update 2

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default function navbar() {
-  const handleSubmit = (e) => {
+export default function Navbar() {
+  const [inputValue, setinputValue] = useState('');
+
+  const handleOnChange = (e) => {
+    setinputValue(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('itborks');
+
+    const res = await fetch(`/api/todo/search?filter=${inputValue}`);
+    const json = await res.json();
+    console.log(json);
+
+    setinputValue('');
+    window.location.href = `/search?filter=${inputValue}`;
   };
 
   return (
     <nav className="navbar">
       <form className="form-v1 " onSubmit={handleSubmit}>
-        <input type="text" placeholder="Search" />
-        <input aria-label="search" type="submit" value="" />
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={handleOnChange}
+          value={inputValue}
+        />
+        <input aria-label="search" type="submit" value=""></input>
       </form>
       <div className="navbar__link-container">
         <NavLink to="/" className="link-style-v1">
