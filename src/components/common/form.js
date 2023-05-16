cb8769c73ad1295b0fc4520117685c832e68bf92
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-05-16 12:06:27 +0200

    preattier update

--- src/components/common/form.js
+++ src/components/common/form.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 export default function Form({ fetchTasks }) {
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const mssg = {
       title: title,
       desc: description
-    }
+    };
 
     await fetch('/api/todo/', {
       method: 'POST',
@@ -19,39 +19,31 @@ export default function Form({ fetchTasks }) {
       headers: {
         'Content-Type': 'application/json'
       }
-    })
+    });
 
-    fetchTasks()
-    setTitle('')
-    setDescription('')
-  }
+    fetchTasks();
+    setTitle('');
+    setDescription('');
+  };
 
   return (
-    <form className='create-task-form' onSubmit={handleSubmit}>
-      <label htmlFor='title'>
-          Title
-      </label>
-      <input 
-        placeholder='Task title*' 
-        id='title'
+    <form className="create-task-form" onSubmit={handleSubmit}>
+      <label htmlFor="title">Title</label>
+      <input
+        placeholder="Task title*"
+        id="title"
         value={title}
         required
-        onChange={e => setTitle(e.target.value)}
+        onChange={(e) => setTitle(e.target.value)}
       />
-      <label htmlFor='description'>
-          Description
-      </label>
-      <textarea 
-        placeholder='Task description' 
-        id='description'
+      <label htmlFor="description">Description</label>
+      <textarea
+        placeholder="Task description"
+        id="description"
         value={description}
-        onChange={e => setDescription(e.target.value)}
-      />   
-      <input 
-        type='submit' 
-        className='create-task-form__submit'
+        onChange={(e) => setDescription(e.target.value)}
       />
+      <input type="submit" className="create-task-form__submit" />
     </form>
-  )
+  );
 }
-
