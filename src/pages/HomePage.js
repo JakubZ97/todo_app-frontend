247ff5ed9dbe96a684ff7617a7471ec0f9f51983
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-05-23 11:20:36 +0200

    home page update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,24 @@
 import Tasks from '../components/common/Tasks';
 
-export default function HomePage({ tasks, formatedDate }) {
-  return <Tasks tasks={tasks} formatedDate={formatedDate} />;
+export default function HomePage({
+  tasks,
+  formatedDate,
+  fetchTasks,
+  setTasks,
+  loadingScreen
+}) {
+  return (
+    <main>
+      {loadingScreen ? (
+        <div>loading</div>
+      ) : (
+        <Tasks
+          tasks={tasks}
+          formatedDate={formatedDate}
+          fetchTasks={fetchTasks}
+          setTasks={setTasks}
+        />
+      )}
+    </main>
+  );
 }
