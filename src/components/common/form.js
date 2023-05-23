e51663325b26b49c60d2ebe7f07f9bf6ebd0a732
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-05-23 13:22:42 +0200

    updated form - create task

--- src/components/common/form.js
+++ src/components/common/form.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function Form({ fetchTasks }) {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const [showError, setShowError] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const mssg = {
       title: title,
-      desc: description
+      desc: description.split(/\r?\n|\r|\n/g).join('\n')
     };
+
+    console.log(description.split(/\r?\n|\r|\n/g));
+
+    if (!title.split(' ').join('')) {
+      setShowError(true);
+      return;
+    }
 
     await fetch('/api/todo/', {
       method: 'POST',
@@ -21,19 +27,21 @@ export default function Form({ fetchTasks }) {
       }
     });
 
+    setShowError(false);
     fetchTasks();
     setTitle('');
     setDescription('');
   };
 
   return (
-    <form className="create-task-form" onSubmit={handleSubmit}>
+    <form className="form-v2" onSubmit={handleSubmit}>
       <label htmlFor="title">Title</label>
+      {showError ? <div className="error-message">Title is required</div> : ''}
       <input
         placeholder="Task title*"
         id="title"
         value={title}
-        required
+        // required
         onChange={(e) => setTitle(e.target.value)}
       />
       <label htmlFor="description">Description</label>
@@ -41,9 +49,16 @@ export default function Form({ fetchTasks }) {
         placeholder="Task description"
         id="description"
         value={description}
-        onChange={(e) => setDescription(e.target.value)}
+        onChange={(e) => {
+          setDescription(e.target.value);
+          console.log(description);
+        }}
       />
-      <input type="submit" className="create-task-form__submit" />
+      <input
+        type="submit"
+        className="create-task-form__submit"
+        value="Create Task"
+      />
     </form>
   );
 }
