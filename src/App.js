9b947f7c21708396da22c3d6b170d9a3e5f75ba5
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-05-24 12:35:13 +0200

    App.js update

--- src/App.js
+++ src/App.js
@@ -4,46 +4,71 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import HomePage from './pages/HomePage';
 import CreateTaskPage from './pages/CreateTaskPage';
 import EditTaskPage from './pages/EditTaskPage';
+import SearchPage from './pages/SearchPage';
+
+import { ErrorContext } from './contexts/ErrorContext';
 
 import RootLayout from './layouts/RootLayout';
 
 function App() {
+  const isFirstRender = useRef(true);
+  const [loadingScreen, setLoadingScreen] = useState(true);
   const [tasks, setTasks] = useState([]);
-  const [error, setError] = useState({});
+  const [filteredTasks, setFilteredTasks] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    fetchTasks();
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      fetchTasks();
+    }
   }, []);
 
   const fetchTasks = async () => {
+    setLoadingScreen(true);
+
     const response = await fetch('/api/todo');
+
+    setTasks([]);
 
     if (response.ok) {
       const jsonData = await response.json();
       setTasks(jsonData);
+      setLoadingScreen(false);
     } else {
       const { status, statusText } = response;
       setError({ status, statusText });
     }
   };
 
-  const formatedDate = (oldDate) => {
-    const date = new Date(oldDate);
+  const formatedDate = (date) => {
+    const newDate = new Date(date);
 
-    return date.toLocaleString();
+    return newDate.toLocaleString();
   };
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<RootLayout />}>
+      <Route
+        path="/"
+        element={<RootLayout setFilteredTasks={setFilteredTasks} />}
+      >
         <Route
           index
-          element={<HomePage tasks={tasks} formatedDate={formatedDate} />}
+          element={
+            <HomePage
+              tasks={tasks}
+              formatedDate={formatedDate}
+              fetchTasks={fetchTasks}
+              setTasks={setTasks}
+              loadingScreen={loadingScreen}
+            />
+          }
         />
         <Route
           path="/create-task"
@@ -51,17 +76,29 @@ function App() {
         />
         <Route
           path="/:id"
-          element={<EditTaskPage formatedDate={formatedDate} />}
+          element={
+            <EditTaskPage formatedDate={formatedDate} fetchTasks={fetchTasks} />
+          }
         ></Route>
         <Route
           path="/search"
-          element={<CreateTaskPage fetchTasks={fetchTasks} />}
+          element={
+            <SearchPage
+              filteredTasks={filteredTasks}
+              formatedDate={formatedDate}
+              fetchTasks={fetchTasks}
+            />
+          }
         />
       </Route>
     )
   );
 
-  return <RouterProvider router={router} />;
+  return (
+    <ErrorContext.Provider value={{ error, setError }}>
+      <RouterProvider router={router} />
+    </ErrorContext.Provider>
+  );
 }
 
 export default App;
