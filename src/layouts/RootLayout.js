042479c8122c7be92b9dfa24ba5a189f2248a560
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-05-25 12:36:28 +0200

    RootLayout.js update

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Header from '../components/layout/Header';
+import Footer from '../components/layout/Footer';
+import ErrorPopUp from '../components/common/ErrorPopUp';
 
-export default function RootLayout() {
+import { TaskContext } from '../contexts/TaskContext';
+
+export default function RootLayout({ setFilteredTasks }) {
+  const [editTask, setEditTask] = useState({});
+
   return (
-    <div className="root-layout">
-      <Header />
-      <main>
+    <>
+      <Header setFilteredTasks={setFilteredTasks} />
+      <TaskContext.Provider value={{ editTask, setEditTask }}>
         <Outlet />
-      </main>
-    </div>
+      </TaskContext.Provider>
+      <Footer />
+      <ErrorPopUp />
+    </>
   );
 }
