4bc0e6d4c903ffef2392a1377a309b35ac0f4d3e
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-05-25 12:34:49 +0200

    reevaluated task component

--- src/components/common/Task.js
+++ src/components/common/Task.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import editSVG from '../../assets/edit.svg';
 import deleteSVG from '../../assets/delete.svg';
+import { TaskContext } from '../../contexts/TaskContext';
 
 export default function Task({ task, handleDelete, formatedDate, index }) {
+  const { setEditTask } = useContext(TaskContext);
+
+  const handleSetEditTask = () => {
+    setEditTask(task);
+  };
+
   return (
     <div className="task">
       <div className="task__content-container">
@@ -14,19 +21,27 @@ export default function Task({ task, handleDelete, formatedDate, index }) {
         {task.desc ? <p className="task__description">{task.desc}</p> : ''}
         <time className="task__timestamp">{formatedDate(task.createdAt)}</time>
       </div>
-      <div className="task__button-container">
-        <Link to={task._id} className="task__edit-button">
-          <img src={editSVG} alt="Edit"></img>
-        </Link>
-        <button
-          className="task__delete-button"
-          onClick={(e) => {
-            handleDelete(e, task._id, index);
-          }}
-        >
-          <img src={deleteSVG} alt="Delete"></img>
-        </button>
-      </div>
+      {handleDelete ? (
+        <div className="task__button-container">
+          <Link
+            to={task._id}
+            className="task__edit-button"
+            onClick={handleSetEditTask}
+          >
+            <img src={editSVG} alt="Edit"></img>
+          </Link>
+          <button
+            className="task__delete-button"
+            onClick={(e) => {
+              handleDelete(e, task._id, index);
+            }}
+          >
+            <img src={deleteSVG} alt="Delete"></img>
+          </button>
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   );
 }
